5880e11f55a6e9eb8d9de2c0b41ef55dc7a24c0e
Pixwok <40206880+Pixwok@users.noreply.github.com>
2025-10-31 19:00:31 +0100

    Système de saison

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 import json
 import jmespath
+from time import *
 
+## Demande nombre de plat
+nbrPlat = int(input("Combien de plat voulez vous générer : "))
+mode = input("Voulez vous des plats de saison Y/N : ")
+
+def saison(month):
+    if (month > 9 and month <=12) or (month >=1 and month < 4):
+        return "hiver"
+    else:
+        return "été"
+
+# Chargemetn fichier JSON
 meals = open("meals.json", 'r', encoding='utf-8')
-
 data = json.load(meals)
 
-platSaison = jmespath.search("[?(season == 'all' || season == 'été')].name", data['meals'])
+## Mode saison
+if mode.upper() == "Y":
+    saison = saison(int(strftime("%m", localtime())))
+    platSaison = jmespath.search("[?(season == 'all' || season == '"+ saison +"')].name", data['meals'])
+else:
+    platSaison = jmespath.search("[].name", data['meals'])
 
 print(platSaison)
