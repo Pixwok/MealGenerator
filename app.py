ffc7cc9a2227bf4c1b4a533de1ccfd5f1aee3c6e
pixwok <dev@pixwok.fr>
2025-11-03 12:20:59 +0100

    refacto

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import jmespath
 import random
 from time import *
 
-def saison(month):
+def getSaison(month):
     if (month > 9 and month <=12) or (month >=1 and month < 4):
         return "hiver"
     else:
@@ -29,24 +29,44 @@ def listePlat(platList):
             filePlat.write("\n\t" + ingredient)
     filePlat.close()
 
+def GenerateMeals(ingredient = ""):
+    # Chargement fichier JSON
+    meals = open("meals.json", 'r', encoding='utf-8')
+    data = json.load(meals)
+    # Vérif
+    if ingredient != "":
+        print(ingredient)
+    else:
+        ## Demande nombre de plat
+        nbrPlat = int(input("Combien de plat voulez vous générer : "))
+        mode = input("Voulez vous des plats de saison Y/N : ")
+        # Mode saison
+        if mode.upper() == "Y":
+            saison = getSaison(int(strftime("%m", localtime())))
+            platSaison = jmespath.search("[?(season == 'all' || season == '"+ saison +"')]", data['meals'])
+            platList = random.sample(platSaison, k=nbrPlat) ## Tirage sans remise
+        else:
+            platList = random.sample(data['meals'], k=nbrPlat) ## Tirage sans remise
+        listePlat(platList)
+        listeCourse(platList)
 
-## Demande nombre de plat
-nbrPlat = int(input("Combien de plat voulez vous générer : "))
-mode = input("Voulez vous des plats de saison Y/N : ")
-
-# Chargement fichier JSON
-meals = open("meals.json", 'r', encoding='utf-8')
-data = json.load(meals)
-
-## Mode saison
-if mode.upper() == "Y":
-    saison = saison(int(strftime("%m", localtime())))
-    platSaison = jmespath.search("[?(season == 'all' || season == '"+ saison +"')]", data['meals'])
-    platList = random.sample(platSaison, k=nbrPlat) ## Tirage sans remise
-else:
-    platList = random.sample(data['meals'], k=nbrPlat) ## Tirage sans remise
-
-
-listePlat(platList)
-listeCourse(platList)
-
+while (True):
+    print("=====MealGenerator=====")
+    print("\r1.Générer des repas")
+    print("\r2.Lister les repas")
+    print("\r3.Lister repas à partir d'un ingrédient")
+    print("\r9.Exit")
+    choisse = int(input("Tapper le numéro de l'action: "))
+    
+    match choisse:
+        case 1:
+            GenerateMeals()
+        case 2:
+            print("2")
+        case 3:
+            ingredient = input("Choisir un ingrédient: ")
+            print(GenerateMeals(ingredient))
+        case 9:
+            break
+        case _:
+            GenerateMeals()
